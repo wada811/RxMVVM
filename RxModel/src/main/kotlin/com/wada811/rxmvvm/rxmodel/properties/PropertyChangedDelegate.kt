2ad8e8d0f813573af28e763092b591c357fb400c
wada811 <at.wada811@gmail.com>
2017-03-02 07:33:23 +0900

    PropertyChangedDelegate implements ReadWriteProperty

--- RxModel/src/main/kotlin/com/wada811/rxmvvm/rxmodel/properties/PropertyChangedDelegate.kt
+++ RxModel/src/main/kotlin/com/wada811/rxmvvm/rxmodel/properties/PropertyChangedDelegate.kt
@@ -1,14 +1,15 @@
 package com.wada811.rxmvvm.rxmodel.properties
 
 import com.wada811.rxmvvm.rxmodel.RxModelSubjects
+import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class PropertyChangedDelegate<T>(initialValue: T) {
+class PropertyChangedDelegate<T>(initialValue: T) : ReadWriteProperty<NotifyPropertyChanged, T> {
     private var value: T = initialValue
     
-    operator fun getValue(thisRef: NotifyPropertyChanged, property: KProperty<*>): T = value
+    override operator fun getValue(thisRef: NotifyPropertyChanged, property: KProperty<*>): T = value
     
-    operator fun setValue(thisRef: NotifyPropertyChanged, property: KProperty<*>, value: T) {
+    override operator fun setValue(thisRef: NotifyPropertyChanged, property: KProperty<*>, value: T) {
         if (this.value != value) {
             this.value = value
             notifyPropertyChanged(thisRef, property, value)
